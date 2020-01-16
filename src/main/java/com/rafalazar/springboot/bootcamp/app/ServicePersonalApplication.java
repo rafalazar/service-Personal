25ead5f37dc3cbf75abfdbba12d5d4571ec9dcdb
Jordan <jcr.salazar@outlook.com>
2020-01-15 21:18:56 -0500

    Se ha añadido el proyecto a Eureka server.

--- src/main/java/com/rafalazar/springboot/bootcamp/app/ServicePersonalApplication.java
+++ src/main/java/com/rafalazar/springboot/bootcamp/app/ServicePersonalApplication.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 
 import com.rafalazar.springboot.bootcamp.app.service.PersonalService;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ServicePersonalApplication implements CommandLineRunner{
 	
